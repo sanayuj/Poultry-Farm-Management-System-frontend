1192b0edbb279daa95bfc92af369ff978356a5a8
sanayuj <sanayuj2255@gmail.com>
2023-12-15 15:01:18 +0530

    mortality details store in db

--- src/Routers/userRouters.js
+++ src/Routers/userRouters.js
@@ -7,6 +7,8 @@ import AddFarm from "../Pages/User/AddFarmPage";
 import FeedManagePage from "../Pages/User/FeedManagePage";
 import ShowFarmsPage from "../Pages/User/ShowFarmsPage";
 import MedicinePage from "../Pages/User/MedicinePage";
+import Mortality from "../Components/User/Mortality/Mortality";
+import MortalityPage from "../Pages/User/MortalityPage";
 
 function UserRouters() {
   return (
@@ -17,7 +19,8 @@ function UserRouters() {
       <Route path="/addfarm" element={<AddFarm />} />
       <Route path="/feed/:farmId" element={<FeedManagePage />} />
       <Route path="/showfarms/:values" element={<ShowFarmsPage />} />
-      <Route path="/medicine/:farmId" element={<MedicinePage/>}/>
+      <Route path="/medicine/:farmId" element={<MedicinePage />} />
+      <Route path="/mortality/:farmId" element={<MortalityPage />} />
     </Routes>
   );
 }
--- src/Services/userApi.js
+++ src/Services/userApi.js
@@ -1,6 +1,5 @@
 import { userInstance } from "../Axios/axiosInstance";
 
-
 //POST methods
 
 export const userSignup = (values) => {
@@ -11,34 +10,53 @@ export const userLogin = (values) => {
   return userInstance.post("/login", { ...values });
 };
 
-export const addFarmDetails=(values,userId)=>{
-  console.log(userId,"function user Api")
-  return userInstance.post(`/addfarm/${userId}`,{...values})
-}
+export const addFarmDetails = (values, userId) => {
+  console.log(userId, "function user Api");
+  return userInstance.post(`/addfarm/${userId}`, { ...values });
+};
 
-export const addFeedDetails=(values,userId,farmId)=>{
-  return userInstance.post(`/addfeed/${userId}`,{...values,farmId:farmId})
-}
+export const addFeedDetails = (values, userId, farmId) => {
+  return userInstance.post(`/addfeed/${userId}`, { ...values, farmId: farmId });
+};
 
-export const addMedicineDetails=(values,userId,farmId)=>{
-  return userInstance.post(`/addmedicine/${userId}`,{...values,farmId:farmId})
-}
+export const addMedicineDetails = (values, userId, farmId) => {
+  return userInstance.post(`/addmedicine/${userId}`, {
+    ...values,
+    farmId: farmId,
+  });
+};
 
+export const addMortality = (values, userId, farmId) => {
+  return userInstance.post(`/addmortality/${userId}`, {
+    ...values,
+    farmId: farmId,
+  });
+};
 
 //GET methods
 
-export const userHeader=()=>{
-  return userInstance.get('/')
-} 
+export const userHeader = () => {
+  return userInstance.get("/");
+};
 
-export const showUserFarms=(userId)=>{
-  return userInstance.get(`/showuserfarms/${userId}`)
-}
+export const showUserFarms = (userId) => {
+  return userInstance.get(`/showuserfarms/${userId}`);
+};
 
-export const showFeedDetails=(userId,farmId)=>{
-  return userInstance.get(`/feedDetails/${userId}`,{ params: { farmId: farmId } })
-}
+export const showFeedDetails = (userId, farmId) => {
+  return userInstance.get(`/feedDetails/${userId}`, {
+    params: { farmId: farmId },
+  });
+};
 
-export const showMedicineDetails=(farmId,userId)=>{
-  return userInstance.get(`/medicineDetails/${userId}`,{params:{farmId:farmId}})
-}
+export const showMedicineDetails = (farmId, userId) => {
+  return userInstance.get(`/medicineDetails/${userId}`, {
+    params: { farmId: farmId },
+  });
+};
+
+export const showMortality = (farmId, userId) => {
+  return userInstance.get(`/mortalityDetails/${userId}`, {
+    params: { farmId: farmId },
+  });
+};
